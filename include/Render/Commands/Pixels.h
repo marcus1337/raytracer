589fab61bc48557bcd59397c37907a2731b49823
marcus1337 <blurgas@hotmail.com>
2024-05-31 12:51:47 +0200

    render canvas

--- include/Render/Commands/Pixels.h
+++ include/Render/Commands/Pixels.h
@@ -8,6 +8,13 @@ namespace rt
 {
     namespace cmd
     {
+
+        struct Pixel
+        {
+            Point position;
+            Color color;
+        };
+
         class Pixels : public RenderCommand
         {
         public:
@@ -16,20 +23,48 @@ namespace rt
             }
             virtual void render(SDL_Renderer *renderer)
             {
-                for (int x = 0; x < canvas.getWidth(); x++)
-                {
-                    for (int y = 0; y < canvas.getHeight(); y++)
-                    {
-                        const auto color = canvas.at(x, y);
-                        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-                        SDL_RenderDrawPoint(renderer, position.x + x, position.y + y);
-                    }
-                }
+                SDL_BlendMode oldBlendMode;
+                SDL_GetRenderDrawBlendMode(renderer, &oldBlendMode);
+                SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+
+                renderPixels(renderer);
+
+                SDL_SetRenderDrawBlendMode(renderer, oldBlendMode);
             }
 
         private:
             const Canvas canvas;
             const rt::Point position;
+
+            std::vector<Pixel> getPixels() const
+            {
+                std::vector<Pixel> pixels;
+                for (int x = 0; x < canvas.getWidth(); x++)
+                {
+                    for (int y = 0; y < canvas.getHeight(); y++)
+                    {
+                        pixels.push_back(Pixel{.position = Point(x, y), .color = canvas.at(x, y)});
+                    }
+                }
+                return pixels;
+            }
+
+            void renderPixel(SDL_Renderer *renderer, const Pixel &pixel) const
+            {
+                const auto &color = pixel.color;
+                const std::size_t x = pixel.position.x;
+                const std::size_t y = pixel.position.y;
+                SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+                SDL_RenderDrawPoint(renderer, position.x + x, position.y + y);
+            }
+
+            void renderPixels(SDL_Renderer *renderer) const
+            {
+                for (const auto &pixel : getPixels())
+                {
+                    renderPixel(renderer, pixel);
+                }
+            }
         };
     }
 }
