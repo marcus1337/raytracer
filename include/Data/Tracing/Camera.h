b6feb4e6ffc6cdbba36abdb025884d839e16b3d0
marcus1337 <blurgas@hotmail.com>
2024-06-02 12:05:23 +0200

    spawn ray structure

--- include/Data/Tracing/Camera.h
+++ include/Data/Tracing/Camera.h
@@ -9,8 +9,31 @@ namespace rt
     {
     public:
         Transform transform;
-        virtual ~Camera();
+        virtual ~Camera() = default;
         virtual Ray spawnRay(float u, float v) const = 0;
+
+    protected:
+    private:
+    };
+
+    class OrthogonalCamera : public Camera
+    {
+    public:
+        virtual Ray spawnRay(float u, float v) const override
+        {
+            return Ray(); // TODO
+        }
+
+    private:
+    };
+
+    class PerspectiveCamera : public Camera
+    {
+    public:
+        virtual Ray spawnRay(float u, float v) const override
+        {
+            return Ray(); // TODO
+        }
 
     private:
     };
