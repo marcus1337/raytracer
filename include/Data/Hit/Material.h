2f39404c3bb892b9d14c3a14a04301e1f9b6e065
marcus1337 <blurgas@hotmail.com>
2024-06-07 07:12:52 +0200

    matutil

--- include/Data/Hit/Material.h
+++ include/Data/Hit/Material.h
@@ -13,6 +13,13 @@ namespace rt
         glm::vec3 attenuation; // color
     };
 
+    namespace MatUtil
+    {
+        extern thread_local RayRand rayRand;
+        bool nearZero(const glm::vec3 &v);
+        glm::vec3 reflect(const glm::vec3 &v, const glm::vec3 &n);
+    }
+
     class Material
     {
     public:
@@ -20,41 +27,15 @@ namespace rt
         virtual std::optional<Scatter> scatter(const Ray &rIn, const HitLocation &loc) const = 0;
 
     protected:
-        static thread_local RayRand rayRand;
-
-        bool nearZero(const glm::vec3 &v) const
-        {
-            const float epsilon = 1e-8;
-            return glm::all(glm::lessThan(glm::abs(v), glm::vec3(epsilon)));
-        }
-
-        glm::vec3 reflect(const glm::vec3 &v, const glm::vec3 &n) const
-        {
-            return v - 2 * glm::dot(v, n) * n;
-        }
-
     private:
     };
 
     class Lambertian : public Material
     {
     public:
-        Lambertian(const glm::vec3 &albedo) : albedo(albedo)
-        {
-        }
+        Lambertian(const glm::vec3 &albedo);
 
-        virtual std::optional<Scatter> scatter(const Ray &rIn, const HitLocation &loc) const override
-        {
-            Scatter scat;
-            auto scatDir = loc.normal + rayRand.randUnitVec();
-            if (nearZero(scatDir))
-            {
-                scatDir = loc.normal;
-            }
-            scat.r = Ray(loc.p, scatDir);
-            scat.attenuation = albedo;
-            return scat;
-        }
+        virtual std::optional<Scatter> scatter(const Ray &rIn, const HitLocation &loc) const override;
 
     private:
         glm::vec3 albedo; // Color
@@ -63,24 +44,9 @@ namespace rt
     class Metal : public Material
     {
     public:
-        Metal(const glm::vec3 &albedo, float fuzz) : albedo(albedo), fuzz(fuzz)
-        {
-        }
+        Metal(const glm::vec3 &albedo, float fuzz);
 
-        virtual std::optional<Scatter> scatter(const Ray &rIn, const HitLocation &loc) const override
-        {
-            Scatter scat;
-            auto reflected = reflect(rIn.direction(), loc.normal);
-            reflected = glm::normalize(reflected) + (fuzz * rayRand.randUnitVec());
-            scat.r = Ray(loc.p, reflected);
-            scat.attenuation = albedo;
-            if (glm::dot(scat.r.direction(), loc.normal) <= 0)
-            {
-                return std::nullopt;
-            }
-
-            return scat;
-        }
+        virtual std::optional<Scatter> scatter(const Ray &rIn, const HitLocation &loc) const override;
 
     private:
         glm::vec3 albedo; // Color
--- src/Data/Material.cpp
+++ src/Data/Material.cpp
@@ -1,5 +1,58 @@
 #include "Data/Hit/Material.h"
 
-using namespace rt;
+namespace rt
+{
 
- thread_local RayRand Material::rayRand;
+    namespace MatUtil
+    {
+        thread_local RayRand rayRand;
+
+        bool nearZero(const glm::vec3 &v)
+        {
+            const float epsilon = 1e-8;
+            return glm::all(glm::lessThan(glm::abs(v), glm::vec3(epsilon)));
+        }
+
+        glm::vec3 reflect(const glm::vec3 &v, const glm::vec3 &n)
+        {
+            return v - 2 * glm::dot(v, n) * n;
+        }
+    }
+
+    Lambertian::Lambertian(const glm::vec3 &albedo) : albedo(albedo)
+    {
+    }
+
+    std::optional<Scatter> Lambertian::scatter(const Ray &rIn, const HitLocation &loc) const
+    {
+        Scatter scat;
+        auto scatDir = loc.normal + MatUtil::rayRand.randUnitVec();
+        if (MatUtil::nearZero(scatDir))
+        {
+            scatDir = loc.normal;
+        }
+        scat.r = Ray(loc.p, scatDir);
+        scat.attenuation = albedo;
+        return scat;
+    }
+
+    Metal::Metal(const glm::vec3 &albedo, float fuzz) : albedo(albedo), fuzz(fuzz)
+    {
+    }
+
+    std::optional<Scatter> Metal::scatter(const Ray &rIn, const HitLocation &loc) const
+    {
+        Scatter scat;
+        auto reflected = reflect(rIn.direction(), loc.normal);
+        reflected = glm::normalize(reflected) + (fuzz * MatUtil::rayRand.randUnitVec());
+        scat.r = Ray(loc.p, reflected);
+        scat.attenuation = albedo;
+        if (glm::dot(scat.r.direction(), loc.normal) <= 0)
+        {
+            return std::nullopt;
+        }
+
+        return scat;
+    }
+
+}
